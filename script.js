488123b18be4360a32515405c2dc53da0ff105ff
gcohen1 <156716839+gcohen1@users.noreply.github.com>
2024-02-17 20:29:38 +0000

    new changes

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ function setup(){
 }
 
 function draw(){
-  background(0,0,184);
+  background(195,0,184);
   // add your drawing code here ...
   fill(155,0,0)
-  ellipse (150,50,50)
-  triangle(150,25,25)
-  line(160,20,20)
-  line(150,22,22)
+  ellipse(width/2, height/2, 50); // head
+  ellipse(width/2 - 20, height/2, - 10, 10); // left eye
+  ellipse(width/2 + 20, height/2, - 10, 10); // right eye
+  line(width/2 - 15, height/2, - 30, height/2); // left ear
+  line(width/2 + 15, height/2, + 30, height/2); // left ear
+  triangle(width/2 - 5, height/2, width/2 + 5, height/2, width/2, height/2 + 8); // nose
+}
 }
