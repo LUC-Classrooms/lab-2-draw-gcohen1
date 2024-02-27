775cd58dec1d2d94b84821f619c82c4ef8349d15
gcohen1 <156716839+gcohen1@users.noreply.github.com>
2024-02-27 17:01:57 +0000

    eyes adjusted

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ function draw(){
   fill(155,0,0)
   noStroke();
   ellipse(width/2, height/2, 50); // head
+
   fill ("pink");
-  ellipse(width/2 - 20, height/2, - 10, 10); // left eye
-  ellipse(width/2 + 20, height/2, - 10, 10); // right eye
-  fill ("blue");
-  line(width/2 - 15, height/2, - 30, height/2); // left ear
-  line(width/2 + 15, height/2, + 30, height/2); // left ear
+  ellipse(width/2 - 15, height/2, - 10, 10); // left eye
+  ellipse(width/2 + 15, height/2, - 10, 10); // right eye
+
   fill ("black"); 
   strokeWeight (5);
-  triangle(width/2 - 5, height/2, width/2 + 5, height/2, width/2, height/2 + 8); // nose
+  triangle(width/2 - 5, height/2, width/2 + 5, height/2, width/2, height/2 + 6); // nose
   stroke (0); 
-  ellipse(width/2 - 20, height/2 - 10, 13); // mouth
+
+  ellipse(width/2 - 0, height/2 + 14 , 6); // mouth
 
 }
 
